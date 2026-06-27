85328747b83544ac206cfe6f3fa2ca7cfce73473
ananya2501020074 <2501020074@mail.jiit.ac.in>
2026-06-27 11:19:59 +0530

    updated main for windows

--- main.c
+++ main.c
@@ -10,4 +10,27 @@ int user_Quiz(MYSQL *conn, const char *server, const char *user, const char *pas
 void about(const char *name);
 void leaderboard(MYSQL *conn);
 int questions();
+int main() {
+  MYSQL *conn;
+  const char *server = "localhost";
+  const char *user = "root";
+  char password[20];
+  const char *database = NULL; // FIXED: Must be NULL, not uninitialized garbage
 
+  conn = mysql_init(NULL);
+
+  printf("Enter the password : ");
+  scanf("%19s", password);
+
+  printf("Enter the no of players participating : ");
+  int n;
+  scanf("%d", &n);
+  getchar(); // Clears trailing newline
+
+  user_Entry(conn, server, user, password, database, n);
+  user_Quiz(conn, server, user, password, database, n);
+  leaderboard(conn);
+
+  mysql_close(conn);
+  return EXIT_SUCCESS;
+}
